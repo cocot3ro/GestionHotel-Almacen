ea1cfe2033ea15e758a94dc402b84d3df12e9b23
cocot3ro <pablooterocastro15@gmail.com>
2025-05-24 12:46:37 +0200

    feat: Add logOut functionality to MainViewModel
    This commit introduces a `logOut` function to the `MainViewModel`.
    
    - Added `logOut` suspend function to `MainViewModel`.
    - This function calls the `logOut` method of the `ManageLoginUsecase`.
    
    Signed-off-by: cocot3ro <pablooterocastro15@gmail.com>

---
+++ composeApp/src/commonMain/kotlin/com/cocot3ro/gh/almacen/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.cocot3ro.gh.almacen
+
+import androidx.lifecycle.ViewModel
+import com.cocot3ro.gh.almacen.domain.usecase.ManageLoginUsecase
+import org.koin.android.annotation.KoinViewModel
+import org.koin.core.annotation.Provided
+
+@KoinViewModel
+class MainViewModel(
+    @Provided private val manageLoginUsecase: ManageLoginUsecase
+) : ViewModel() {
+
+    suspend fun logOut() {
+//        viewModelScope.launch(Dispatchers.IO) {
+        manageLoginUsecase.logOut()
+//        }
+    }
+}

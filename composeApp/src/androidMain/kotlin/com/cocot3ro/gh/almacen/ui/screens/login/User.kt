2ace4b43455954754547394826927befad071aed
cocot3ro <pablooterocastro15@gmail.com>
2025-06-04 02:53:37 +0200

    Refactor: Improve User Card UI in Login Screen
    This commit enhances the UI of the user cards displayed on the login screen.
    
    **Key Changes:**
    
    - **Layout Adjustments:**
        - Replaced `Box` with `Column` for better alignment and arrangement of elements within the user card.
        - User image now has an aspect ratio of 1f and fills the available width.
        - A placeholder icon (`broken_image_24dp`) is displayed if the user image fails to load.
    - **Text Styling and Content:**
        - User's name is now centered horizontally.
        - If a user `requiresPassword`, a lock icon (`lock_24dp`) is displayed next to their name using `InlineTextContent`.
        - Text alignment is set to `TextAlign.Center`.
        - Font size for the lock icon placeholder is set to `20.sp`.
    
    Signed-off-by: cocot3ro <pablooterocastro15@gmail.com>

--- composeApp/src/androidMain/kotlin/com/cocot3ro/gh/almacen/ui/screens/login/User.kt
+++ composeApp/src/androidMain/kotlin/com/cocot3ro/gh/almacen/ui/screens/login/User.kt
@@ -1,18 +1,32 @@
 package com.cocot3ro.gh.almacen.ui.screens.login
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.cocot3ro.gh.almacen.domain.model.AlmacenUserDomain
+import gh_almacen.composeapp.generated.resources.Res
+import gh_almacen.composeapp.generated.resources.broken_image_24dp
+import gh_almacen.composeapp.generated.resources.lock_24dp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.vectorResource
 
 @Composable
 fun User(
@@ -27,10 +41,10 @@ fun User(
 //        ),
         onClick = onClick
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(ratio = 0.85f)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             if (user.image != null) {
                 AsyncImage(
@@ -38,24 +52,50 @@ fun User(
                         .fillMaxWidth()
                         .aspectRatio(1f),
                     model = user.image,
-                    contentDescription = null
+                    contentDescription = null,
+                    error = painterResource(Res.drawable.broken_image_24dp)
+                )
+            }
+
+            if (!user.requiresPassword) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = user.name,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+            } else {
+                val modId = "lockIcon"
+                val text: AnnotatedString = buildAnnotatedString {
+                    appendInlineContent(id = modId, alternateText = "[icon]")
+                    append(text = user.name)
+                }
+
+                val inlineContent: Map<String, InlineTextContent> = mapOf(
+                    pair = Pair(
+                        first = modId,
+                        second = InlineTextContent(
+                            placeholder = Placeholder(
+                                width = 20.sp,
+                                height = 20.sp,
+                                placeholderVerticalAlign = PlaceholderVerticalAlign.Center
+                            ),
+                            children = {
+                                Icon(
+                                    modifier = Modifier.fillMaxSize(),
+                                    imageVector = vectorResource(Res.drawable.lock_24dp),
+                                    contentDescription = null
+                                )
+                            }
+                        )
+                    )
                 )
 
                 Text(
-                    modifier = Modifier
-                        .padding(top = 4.dp)
-                        .align(Alignment.BottomCenter),
-                    text = user.name,
+                    text = text,
+                    inlineContent = inlineContent,
                     fontWeight = FontWeight.Bold,
-//                    color = Color.Black
-                )
-            } else {
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Center),
-                    text = user.name,
-                    fontWeight = FontWeight.Bold,
-//                    color = Color.Black
+                    textAlign = TextAlign.Center
                 )
             }
         }
